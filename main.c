f0211e528f0bc0f150b6adc88d3d6fe5cc153e83
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-05 23:00:07 -0700

    Create main.c
    start 1

---
+++ main.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#define MAXNUM 10U
+int last=0;
+
+
+
+int main() {
+    // Write C code here
+    
+    int array[MAXNUM]={1,2,3,4,5};
+    last=4;
+    
+    printf("Try programiz.pro _%d",last);
+
+    return 0;
+}
