675509e08ac3a9c2f87e81396ed42eeb00da51b4
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-08 05:27:51 -0700

    Update main.c
    change

--- main.c
+++ main.c
@@ -4,12 +4,32 @@
 int last=0;
 
 
+int insert(int aPos, int* aArray,int aValue)
+{
+    
+    if(aPos>=last)
+    {
+        printf("invalid")
+        return -1;
+    }
+    
+    // for(int i=aPos;i<last;i++)
+    // {
+    //     pos[]=pos[];
+    // }
+    
+    for(int i=(last-1);i>=aPos;i--)
+    {
+        aArray[i]=aArray[i-1];
+    }
+}
+
 
 int main() {
     // Write C code here
+    int array[MAXNUM]={1,2,3,4,5,6,7,8,9,10};
     
-    int array[MAXNUM]={1,2,3,4,5};
-    last=4;
+    last=10;
     
     printf("Try programiz.pro _%d",last);
 
