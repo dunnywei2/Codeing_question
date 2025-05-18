f0621400e304956277e2175573ecfce5dc01f47a
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-18 12:17:54 -0700

    Update circular_buffer.c
    others

--- circular_buffer.c
+++ circular_buffer.c
@@ -1,5 +1,27 @@
+
+/*other people's solution*/
 #include <stdio.h>
 
-typedef struct{
+# define BUFFER SIZE 8
 
+typedef struct {
+uint8 t buffer[BUFFER SIZE];
+int head;
+int tail;
 } CircularBuffer;
+
+ void initBuffer(CircularBuffer * cb) {
+	cb-> head =0;
+	cb-> tail =0;
+ }
+
+void writeBuffer(CircularBuffer * cb, uint8 t data) {
+	cb-> buffer[cb-> head] = data;
+	cb-> head = (cb-> head +1) % BUFFER SIZE;
+ }
+
+uint8 t readBuffer(CircularBuffer * cb) {
+	uint8 t data = cb-> buffer[cb-> tail];
+	cb-> tail = (cb-> tail +1) % BUFFER SIZE;
+	return data;
+	}
