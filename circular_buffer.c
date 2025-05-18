a6bdf4106f1382b0e28ffcb1b9a2a7191de6d8df
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2025-05-18 14:35:21 -0700

    Update circular_buffer.c
    modify

--- circular_buffer.c
+++ circular_buffer.c
@@ -16,6 +16,16 @@ int tail;
 uint8_t counter;
 } CircularBuffer;
 
+
+int isBufferEmpty(CircularBuffer * cb)
+{
+   if((cb->tail==cb->head)&&cb->counter==0))
+   {
+       return 1;
+   }
+   return 0;
+}
+
 int isBufferFull(CircularBuffer * cb)
 {
    if((cb->tail==cb->head)&&(cb->counter==BUFFER_SIZE))
@@ -46,16 +56,26 @@ void writeBuffer(CircularBuffer * cb, uint8_t data) {
  }
 
 uint8_t readBuffer(CircularBuffer * cb) {
+	if(isBufferEmpty(cb))
+	{
+		printf("buffer is empty\n");
+		return -1;
+	}
 	uint8_t data = cb-> buffer[cb-> tail];
 	cb-> tail = (cb-> tail +1) % BUFFER_SIZE;
+	cb->counter--;
+	printf("reading buffer\n");
 	return data;
-	}
+}
 
 int main()
 {
     CircularBuffer cBuffer;
     initBuffer(&cBuffer);
     printf("Hello World first\n");
+
+    for(int i=0;i<2;i++)
+       readBuffer(&cBuffer,i);	
 
     for(int i=0;i<BUFFER_SIZE+1;i++)
        writeBuffer(&cBuffer,i);	
