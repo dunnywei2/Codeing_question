35d5cdf2eae7315c8f3822fb27c48b2727f83057
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2026-07-14 20:43:21 -0400

    Update ring_buffer_v00.c
    continue

--- ring_buffer_v00.c
+++ ring_buffer_v00.c
@@ -1,3 +1,16 @@
-static int full_flag=0; //default as false
+static int full_flag=0U; //default as false
+static int index=0U;
 
+
+
+int write(int aIndex)
+{
+   int return_value=-1;
+   if(full_flag==1) //full
+   {
+     return return_value;
+   }
+   
+   return return_value;
+}
 
