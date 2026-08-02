6d9c6fa8434a5be2b87485f14ce2c65070beb980
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2026-08-02 06:45:50 -0400

    Update ring_buffer_v00.c
    continue

--- ring_buffer_v00.c
+++ ring_buffer_v00.c
@@ -1,5 +1,14 @@
-static int full_flag=0U; //default as false
-static int index=0U;
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int full_flag=0U; //default as false
 
 static int gLength=8;
 
@@ -11,17 +20,17 @@ int r_index=0;
 
 int push(int value,int *array)
 {
-   int return_value=-1;
    if(full_flag==1) //full
    {
      return -1;
    }
+   printf("here 1 \n");
 
-   if(aIndex>=gLength)
-   {
-        full_flag=1;
-        return -1;
-   }
+//   if(aIndex>=gLength)
+//   {
+//         full_flag=1;
+//         return -1;
+//   }
    
    l_index=(l_index+1)%gLength;
    printf("l_index is %d \n\r",l_index);
@@ -33,10 +42,9 @@ int main()
 {
    int a[10];
 
-   for(int i=0;i++;i<10)
+   for(int i=0;i<10;i++)
       {
-         push(i,a);
+        printf("i %d \n",i);
+        push(i,a);
       }
 }
-
-
