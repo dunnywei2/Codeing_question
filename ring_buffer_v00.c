dd2a0d9d99c9c913ddb552f88ee7ac9857d9eaee
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2026-08-02 06:36:25 -0400

    Update ring_buffer_v00.c
    continue

--- ring_buffer_v00.c
+++ ring_buffer_v00.c
@@ -1,12 +1,15 @@
 static int full_flag=0U; //default as false
 static int index=0U;
 
-static int gLength=0;
+static int gLength=8;
 
 int *l=NULL;
 int *r=NULL;
 
-int write(int aIndex,int *array)
+int l_index=0;
+int r_index=0;
+
+int push(int value,int *array)
 {
    int return_value=-1;
    if(full_flag==1) //full
@@ -19,11 +22,21 @@ int write(int aIndex,int *array)
         full_flag=1;
         return -1;
    }
-   l=array
-   l++;
-   return_value=0;
    
-   
-   return return_value;
+   l_index=(l_index+1)%gLength;
+   printf("l_index is %d \n\r",l_index);
+     
+   return 0;
 }
+
+int main()
+{
+   int a[10];
+
+   for(int i=0;i++;i<10)
+      {
+         push(i,a);
+      }
+}
+
 
