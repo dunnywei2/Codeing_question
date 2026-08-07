d7e3cb05fdbc290c657aa89c6e3e9e0462ddabbd
dunnywei2 <146505843+dunnywei2@users.noreply.github.com>
2026-08-06 22:57:39 -0400

    Update ring_buffer_v00.c
    t_index
    h_index

--- ring_buffer_v00.c
+++ ring_buffer_v00.c
@@ -1,20 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <stdio.h>
-
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//https://www.youtube.com/watch?v=uvD9_Wdtjtw&t=5s
 
 #include <stdio.h>
 
@@ -25,8 +9,8 @@ static int gLength=8;
 int *l=NULL;
 int *r=NULL;
 
-int l_index=0;
-int r_index=0;
+int h_index=0;
+int t_index=0;
 
 int push(int value,int *array)
 {
@@ -41,9 +25,9 @@ int push(int value,int *array)
 //         full_flag=1;
 //         return -1;
 //   }
-   array[l_index]=value;
-   l_index=(l_index+1)%gLength;
-   printf("l_index is %d content %d\n\r",l_index,array[l_index]);
+   array[h_index]=value;
+   h_index=(h_index+1)%gLength;
+   printf("h_index is %d content %d\n\r",h_index,array[h_index]);
      
    return 0;
 }
